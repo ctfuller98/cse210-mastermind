2f9c2b4b9eb75c606581995ec98f26bb0ca501d9
Braxton Meyer <braxtonlmeyer@gmail.com>
2021-06-30 17:06:03 -0600

    Added a guess setter and getter, not sure how it communicates with the rest of the program, unfortunately

--- mastermind/game/guess.py
+++ mastermind/game/guess.py
@@ -1,2 +1,10 @@
-class Guess:
-    pass
+class Guess():
+    
+    def __init__(self):
+        self.guess = ""
+
+    def set_guess(self, input_guess):
+        self.guess = input_guess
+
+    def get_guess(self):
+        return self.guess
